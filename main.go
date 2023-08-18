8e5c8cae217c6d9be5e74d06c33f68bf681bcb32
danajoo <danajoo01@gmail.com>
2023-08-18 21:54:36 +0700

    comment loadEnv scalingo

--- main.go
+++ main.go
@@ -3,21 +3,21 @@ package main
 import (
 	"blog-app/configs"
 	"blog-app/routes"
-	"log"
-	"os"
 
-	"github.com/joho/godotenv"
+	//"log"
+	"os"
+	//"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	loadEnv()
+	//loadEnv()
 	configs.InitDB()
 
 	e := echo.New()
 	e = routes.Router(e)
-	e.Start("localhost:" + getPort())
-	//e.Start(":" + getPort())
+	//e.Start("localhost:" + getPort())
+	e.Start(":" + getPort())
 
 }
 
@@ -29,9 +29,9 @@ func getPort() string {
 	return port
 }
 
-func loadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
+//func loadEnv() {
+//	err := godotenv.Load()
+//	if err != nil {
+//		log.Fatal("Error loading .env file")
+//	}
+//}
